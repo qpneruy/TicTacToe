a899dce461855483c92fb9964938dee39fa12550
huuti <huutinh0916@gmail.com>
2024-02-18 10:10:58 +0700

    fkkkkkk

--- src/lib/winLogic.cpp
+++ src/lib/winLogic.cpp
@@ -5,29 +5,32 @@
 #include "include/winLogic.h"
 #include "include/drawScreen.h"
 #include <iostream>
-#include <synchapi.h>
 
 TableData tableData;
 char crossCheck(char intWin, char table[9][9], drawData drawData, char Player) {
     int RcrossCount, LcrossCount;
-    int n = std::max(static_cast<int>(drawData.width), static_cast<int>(drawData.height));
-    for (int i = -n; i <= n; i++) {
+    char n = drawData.intWin;
+    for (int i = -n; i < n; i++) {
         RcrossCount = 0;
         LcrossCount = 0;
-        for (int j = 0; j < n; j++) {
-            if (table[j][n - j + i] == Player) {
-                RcrossCount++;
-                tableData.tablePath_R[RcrossCount].first = i;
-                tableData.tablePath_R[RcrossCount].second = j;
-            } else RcrossCount = 0;
+        for (int j = 0; j < drawData.intWin; j++) {
+            if (abs(n-j+i) <= drawData.intWin) {
+                if (table[j][abs(n-j+i)] == Player) {
+                    RcrossCount++;
+                    tableData.tablePath_R[RcrossCount].first = j;
+                    tableData.tablePath_R[RcrossCount].second = n - j + i;
+                } else RcrossCount = 0;
+            }
             if (table[j][i + j] == Player) {
                 LcrossCount++;
-                tableData.tablePath_L[LcrossCount].first = i;
-                tableData.tablePath_L[LcrossCount].second = j;
+                gotoXY(10, 10);
+                std::cout << j << " " << j + i << " "  << LcrossCount << "\n";
+                tableData.tablePath_L[LcrossCount].first = j;
+                tableData.tablePath_L[LcrossCount].second = i + j;
             } else LcrossCount = 0;
-            if (LcrossCount >= intWin ) {
+            if (RcrossCount >= intWin ) {
                 return 1;
-            } else if (RcrossCount >= intWin) {
+            } else if (LcrossCount >= intWin) {
                 return 2;
             }
         }
@@ -41,7 +44,6 @@ char HorVerCheck(char intWin, char table[9][9], drawData drawData, char Player) 
         for (int j = 0; j < drawData.width; j++) {
             if (table[i][j] == Player) {
                 ++HorCount;
-                std::cout << i << " " << j << "\n";
                 tableData.tablePath_R[HorCount].first = i;
                 tableData.tablePath_R[HorCount].second = j;
             } else HorCount = 0;
@@ -86,14 +88,12 @@ bool isWin(char table[9][9], drawData drData, char Player) {
     if (OresRL != -1) {
         if (OresRL == 1) {
             for (int i = 1; i <= drData.intWin; i++) {
-                table[tableData.tablePath_R[i].second][tableData.tablePath_R[i].first] = '=';
-                std::cout << tableData.tablePath_R[i].first << " " << tableData.tablePath_R[i].second << " \n";
+                table[tableData.tablePath_R[i].first][tableData.tablePath_R[i].second] = '/';
             }
         }
         if (OresRL == 2) {
             for (int i = 1; i <= drData.intWin; i++) {
-                table[tableData.tablePath_L[i].second][tableData.tablePath_L[i].first] = '|';
-                std::cout << tableData.tablePath_L[i].first << " " << tableData.tablePath_L[i].second << " \n";
+                table[tableData.tablePath_L[i].first][tableData.tablePath_L[i].second] = '\\';
             }
         }
         inBanCo(table, drData);
--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ using namespace std;
 void init_(drawData drData, posData &poData) {
     SetWindowSize(95, 34);
     inLogo();
+    inMenuChinh(drData);
     inKhung(78, 34);
 }
 
@@ -36,30 +37,30 @@ void LoadGame(drawData drData, posData poData) {
     inBangThoiGian(drData, 0, 0);
     while (true) {
         getPosMouse(poData);
-        cout << poData.playerPosX << " " << poData.playerPosY;
+//        cout << poData.playerPosX << " " << poData.playerPosY;
         dacPos(drData, poData, poData, px, py);
         if (px != -1 && py != -1 && SyntaxCheck(table, py, px)) {
             gotoXY(5, 21);
             cout << "                 ";
             gotoXY(7, 22);
             cout << "                 ";
-//            if (drData.opr) {
-//                table[py][px] = 'O';
-//                gotoXY(71, drData.y + 10);
-//                cout << 'X';
-//                drData.opr = false;
-//                drData.xpr = true;
-//            } else if (drData.xpr) {
+            if (drData.opr) {
+                table[py][px] = 'O';
+                gotoXY(71, drData.y + 10);
+                cout << 'X';
+                drData.opr = false;
+                drData.xpr = true;
+            } else if (drData.xpr) {
                 table[py][px] = 'X';
                 gotoXY(71, drData.y + 10);
                 cout << 'O';
-//                drData.xpr = false;
-//                drData.opr = true;
-//            }
-//            char currentPlayer = drData.xpr ? 'O' : 'X';
+                drData.xpr = false;
+                drData.opr = true;
+            }
+            char currentPlayer = drData.xpr ? 'O' : 'X';
             inBanCo(table, drData);
             if (isWin(table, drData, 'X')) {
-//                inBanCo(table, drData);
+                inBanCo(table, drData);
                 break;
             }
 
@@ -77,37 +78,27 @@ int main() {
     Player Xplayer{};
     drData.x = 40;
     drData.y = 11;
-    drData.height = 3;
-    drData.width = 3;
+    drData.height = 9;
+    drData.width = 9;
     drData.xpr = true;
-    drData.intWin = 3;
+    drData.intWin = 9;
 
     init_(drData, poData);
     startMap(drData, poData);
-//    gotoXY(0, 0);
-//    for (int i = 0; i <= drData.width; i++) {
-//        std::cout << poData.tableHor[i] << " ";
-//    }
-//    cout << "\n";
-//    for (int i = 0; i <= drData.width; i++) {
-//        std::cout << poData.tableVer[i] << " ";
-//    }
-    LoadGame(drData, poData);
-//    switch (menuHandle(drawData)) {
-//        case 0:
-//
-//            break;
-//        case 1:
-//            UserInput(drawData);
-//
-//            break;
-//        case 2:
-//            break;
-//        case 3:
-//            break;
-//        case 4:
-//            break;
-//    }
+    switch (menuHandle(drData)) {
+        case 0:
+            break;
+        case 1:
+            UserInput(drData);
+            LoadGame(drData, poData);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
     while (true) {
 
     }
